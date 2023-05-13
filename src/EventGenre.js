e70330adb4b9848db88dadcad39b2c8b56d32025
torbalansky <torbalansky@gmail.com>
2023-05-13 13:08:20 +0100

    Attempt to position pie and scatter plots side by side using CSS flexbox

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { PieChart, Pie, Cell, ResponsiveContainer, Legend, Tooltip } from "recharts";
+import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from "recharts";
 
 const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
