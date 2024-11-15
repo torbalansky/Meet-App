41df8daaf47bae0d86b9c5b82193126f640ccb2f
torbalansky <torbalansky@gmail.com>
2024-11-15 11:06:05 +0200

    Update welcome screen.

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -4,6 +4,7 @@ import './WelcomeScreen.css';
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ? (
     <div className="WelcomeScreen">
+      <div className="container">
       <h1>Welcome to the Meet app</h1>
       <h4>Log in to see upcoming events around the world for full-stack developers</h4>
       <div className="button_cont" align="center">
@@ -28,6 +29,7 @@ function WelcomeScreen(props) {
         Privacy policy
       </a>
     </div>
+  </div>
   ) : null;
 }
 
